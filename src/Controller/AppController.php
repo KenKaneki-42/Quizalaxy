607ab7a0d8c5b36f796cb20f04a7e98d58f7f5cc
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-09-21 05:02:44 +0200

    feat: add AppController and home route

---
+++ src/Controller/AppController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AppController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('app/index.html.twig', [
+            'controller_name' => 'AppController',
+        ]);
+    }
+}
