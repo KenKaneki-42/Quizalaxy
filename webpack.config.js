7e242a09495ac79834a5ad287d6a87d0b2ae0070
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-09-21 04:35:05 +0200

    refactor: move app.js to assets/js and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/js/app.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
