a6d55987683b2109a79ae565cf9e75e1c5630d7b
KenKaneki-42 <sylvain.vandermeersch@gmail.com>
2024-09-29 00:45:33 +0200

    build : Add tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html.twig',
+    './assets/**/*.js',
+    './assets/**/*.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
